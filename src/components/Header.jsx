497501f8dadce9e546bd4b8aa4d419fc8dcd4543
Sam Ward-Pickering <samuel.wardpickering@gmail.com>
2022-03-19 16:02:54 +1300

    Style header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="header-container">
+        <h2 className="header-title">TrailBuddy</h2>
+
+
+    </div>
+  )
+}
