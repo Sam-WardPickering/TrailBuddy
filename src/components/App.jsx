cf75f1a2b8a53bf887713ef9ee403ed165c059ff
solidliquidsmokeair <sjgogan@icloud.com>
2022-03-19 16:15:00 +1300

    create HomeBody Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './Header';
+import HomeBody from './HomeBody';
 
 function App() {
   return (
     <>
       <Header/>
+      <HomeBody/>
     </>
   );
 }
